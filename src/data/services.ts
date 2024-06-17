c0fe26821b98ab870a635e31d7d70cb285ac6d60
gerson <garayargerson20@gmail.com>
2024-06-16 19:14:16 -0500

    agregando colores

--- src/data/services.ts
+++ src/data/services.ts
@@ -3,18 +3,18 @@ export const services = [
     title: "Chat con IA",
     description:
       "Integra un chatbot impulsado por inteligencia artificial en tu sitio web para ofrecer asistencia automatizada a tus clientes. Responde preguntas frecuentes, brinda soporte 24/7 y mejora la experiencia del usuario.",
-    image: "",
+    image: "/images/1.png",
   },
   {
     title: "Chat para WhatsApp",
     description:
       "Facilita la comunicación directa con tus clientes a través de WhatsApp. Implementa un chat en tu sitio web que se conecte directamente con tu cuenta de WhatsApp, permitiendo una interacción rápida y eficiente.",
-    image: "",
+    image: "/images/2.png",
   },
   {
     title: "Envíos de Mensajes Masivos por WhatsApp",
     description:
       "Envía mensajes masivos a tus clientes a través de WhatsApp para mantenerlos informados sobre promociones, novedades y actualizaciones. Una herramienta poderosa para campañas de marketing directo y comunicación efectiva.",
-    image: "",
+    image: "/images/3.png",
   },
 ];
