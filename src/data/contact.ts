fe9e661e158f96640d2bd932d7f78d3c176bffb5
gerson <garayargerson20@gmail.com>
2024-07-18 09:31:35 -0500

    feat: agregando estructura de works

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,1 +1,10 @@
-export const contact = ["esalassulca@gmail.com", "+51 927 859 435"];
+export const contact = [
+  {
+    title: "esalassulca@gmail.com",
+    href: "mailto:esalassulca@gmail.com",
+  },
+  {
+    title: "+51 927 859 435",
+    href: "https://wa.me/+51927859435",
+  },
+];
