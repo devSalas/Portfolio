f3c7b2ec04ac29d8d70dd33b25d78abdaa208267
estebans salas sulca <esalassulca@gmail.com>
2024-08-29 12:23:54 -0500

    feat: implement section skills

--- src/data/works.ts
+++ src/data/works.ts
@@ -6,7 +6,24 @@ export const works = [
     video:"video/gigamas/overview.mp4",
     link: "https://gigamas.com.pe",
     tag:"Trabajo profesional",
-    color: "bg-blue-700"
+    color: "bg-blue-700",
+    skill: [
+      {
+          name:"Astro",
+          url:"/skills/astro.png",
+          width:30
+      },
+      {
+          name:"Tailwindcss",
+          url:"/skills/tailwind.png",
+          width:30
+      },
+      {
+          name:"PHP",
+          url:"/skills/php.png"
+      }
+  
+    ]
   },
   {
     title: "Diseño Personalizado para Dentlife, Clínica Dental",
@@ -15,16 +32,103 @@ export const works = [
     video:"video/dentlife/overview.mp4",
     link: "https://dentlifeperu.com",
     tag:"Trabajo profesional",
-    color: "bg-blue-700"
+    color: "bg-blue-700",
+    skill: [
+      {
+          name:"HTML",
+          url:"/skills/html.png",
+          width:30
+      },
+      {
+          name:"Tailwindcss",
+          url:"/skills/tailwind.png",
+          width:30
+      },
+      {
+          name:"JavaScript",
+          url:"/skills/js.png",
+          width:30
+      },
+  
+    ]
   },
   {
-    title: "Diseño Personalizado para Dentlife, Clínica Dental",
+    title: "App de Comida rápida",
     description: "Proyecto en equipo de una aplicación de comida rápida desarrollada desde cero. Este proyecto tiene como base de datos MySQL, en el Backend Node.js, en el Frontend Next.js y esta deployado en un VPS.",
     image: "",
     video:"video/food-app/overview.mp4",
     link: "https://food-app.sunetya.site/",
     tag:"Proyecto Profesional",
-    color:"bg-green-700"
-  }
+    color:"bg-green-700",
+    skill: [
+      {
+          name:"Next.js",
+          url:"/skills/next.png",
+          width:40
+      },
+      {
+          name:"Tailwindcss",
+          url:"/skills/tailwind.png",
+          width:40
+      },
+      {
+          name:"Node.js",
+          url:"/skills/nodejs.png",
+          width:40
+      },
+      {
+          name:"MySql.js",
+          url:"/skills/mysql.png",
+          width:40
+      },
+      {
+          name:"Stripe.js",
+          url:"/skills/stripe.png",
+          width:40
+      },
+  
+    ]
+  },
+  {
+    title: "CompuSalas - Tu Tienda de Componentes de Computadora",
+    description: "E-commerce desarrollado con React, Node.js y MySQL. Compra rápida y segura de componentes de computadora con una interfaz moderna y fácil de usar.",
+    image: "",
+    video:"video/compusalas/overview.mp4",
+    link: "https://compusalas.netlify.app/",
+    tag:"Proyecto Profesional",
+    color:"bg-green-700",
+    skill: [
+      {
+          name:"React.js",
+          url:"/skills/React.png",
+          width:40
+      },
+      {
+          name:"Tailwindcss",
+          url:"/skills/tailwind.png",
+          width:40
+      },
+      {
+          name:"Node.js",
+          url:"/skills/nodejs.png",
+          width:40
+      },
+      {
+          name:"MongoDB.js",
+          url:"/skills/mongo.png",
+          width:40
+      },
+      {
+          name:"React-query",
+          url:"/skills/react-query.png",
+          width:40
+      },
+      {
+          name:"Stripe.js",
+          url:"/skills/stripe.png",
+          width:40
+      },
+  
+    ]  }
 
 ];
