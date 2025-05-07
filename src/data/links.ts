31b3f70117261d7919f5a96c474318475770156f
esalas11 <esalas@totalconfianza.pe>
2025-05-07 16:11:21 -0500

    feat: implement  workingnow section and add profile photo

--- src/data/links.ts
+++ src/data/links.ts
@@ -12,3 +12,5 @@ export const links = [
     href: "/#skills",
   },
 ];
+
+export const LinkedInPost = "https://www.linkedin.com/posts/devsalas_desarrollodesoftware-flutter-bluetooth-activity-7312511806223126528-o1KH?utm_source=share&utm_medium=member_desktop&rcm=ACoAADUd4YMB0DSiBX27JgSlMLTmMxcT8YiBZdM";
